50de70af9da0d392a99a524f3d9235af777db6e2
Leire Ordeñana <leireomadina@gmail.com>
2022-07-31 13:55:56 +0200

    Add styles folder and sass core files

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const router = createRouter({
       path: "/products",
       name: "products",
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (Products.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/ProductsView.vue"),
     },
