5f013e3c8751bad416f6065deef2fa2dc103fc8c
leireomadina <leireomadina@gmail.com>
2023-03-05 17:11:35 +0100

    Add base wrapper component

---
+++ src/components/BaseWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="wrapper">
+		<slot></slot>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+	margin: 0 20px;
+
+	@include desktop {
+		max-width: 1280px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+}
+</style>
