af186dcdba91db58cf87188c7964ef0cf7c71c53
leireomadina <leireomadina@gmail.com>
2023-03-05 17:11:10 +0100

    Fix mixin and refactor

--- src/components/BaseSection.vue
+++ src/components/BaseSection.vue
@@ -1,34 +1,34 @@
 <template>
-  <section :id="id" :class="className">
-    <slot></slot>
-  </section>
+	<section :id="id" :class="className">
+		<slot></slot>
+	</section>
 </template>
 
 <script>
 export default {
-  props: {
-    id: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    className: {
-      type: String,
-      required: false,
-      default: null,
-    },
-  },
-};
+	props: {
+		id: {
+			type: String,
+			required: false,
+			default: null,
+		},
+		className: {
+			type: String,
+			required: false,
+			default: null,
+		},
+	},
+}
 </script>
 
 <style lang="scss" scoped>
 section {
-  padding-top: toRem(80px);
-  padding-bottom: toRem(80px);
+	padding-top: toRem(80px);
+	padding-bottom: toRem(80px);
 
-  @mixin tablet {
-    padding-top: toRem(150px);
-    padding-bottom: toRem(150px);
-  }
+	@include tablet {
+		padding-top: toRem(150px);
+		padding-bottom: toRem(150px);
+	}
 }
 </style>
