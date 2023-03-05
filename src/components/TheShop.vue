7351205a881c2a4d502dc999d6d9a99fd0f00ed5
leireomadina <leireomadina@gmail.com>
2023-03-05 13:42:10 +0100

    Add shop main page component

---
+++ src/components/TheShop.vue
@@ -0,0 +1,12 @@
+<template>
+  <base-section id="shop"> Section content </base-section>
+</template>
+
+<script>
+import BaseSection from "./BaseSection.vue";
+export default {
+  components: {
+    BaseSection,
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <TheHero />
+  <TheShop id="shop" />
 </template>
 
 <script>
 import TheHero from "../components/TheHero.vue";
+import TheShop from "../components/TheShop.vue";
 
 export default {
   components: {
     TheHero,
+    TheShop,
   },
 };
 </script>
