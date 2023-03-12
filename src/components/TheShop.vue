605a9baedd6090dbae8de33afd9bc4245b2ba3bb
leireomadina <leireomadina@gmail.com>
2023-03-12 12:42:37 +0100

    Add shop header component

--- src/components/TheShop.vue
+++ src/components/TheShop.vue
@@ -1,12 +1,21 @@
 <template>
-	<base-section id="shop"> Section content </base-section>
+	<base-section id="shop">
+		<base-wrapper>
+			<TheShopHeader />
+		</base-wrapper>
+	</base-section>
 </template>
 
 <script>
 import BaseSection from './BaseSection.vue'
+import BaseWrapper from './BaseWrapper.vue'
+import TheShopHeader from './TheShopHeader.vue'
+
 export default {
 	components: {
 		BaseSection,
+		TheShopHeader,
+		BaseWrapper,
 	},
 }
 </script>
