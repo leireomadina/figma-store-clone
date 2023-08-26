f87471445f746838c69c856e698c84af519abecd
leireomadina <leireomadina@gmail.com>
2023-08-26 13:09:13 +0200

    Update cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,8 +9,15 @@ describe('My First Test', () => {
 	})
 
 	it('shows the menu when the hamburger button is clicked', () => {
+		cy.get('.menu-modal__list').should('not.exist')
 		cy.get('header').should('not.have.class', 'full-screen')
+
 		cy.get('button.hamburger-menu__btn').click()
+		cy.get('.menu-modal__list').should('exist')
 		cy.get('header').should('have.class', 'full-screen')
+
+		// cy.get('.menu-modal__list').trigger('wheel')
+		// cy.get('.menu-modal__list').should('not.exist')
+		// cy.get('header').should('not.have.class', 'full-screen')
 	})
 })
